9854522a57f5706bb6c9714ea1240adc4d93375b
vagrant <eng.saad.najm@gmail.com>
2022-07-05 19:45:47 +0000

    fixing new line at the end

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,10 @@
 const sentence = "hello there from lighthouse labs";
-let section = 0
+let delay = 0;
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
-  }, section + 100)
+  },delay += 50) 
 };
 setTimeout(() => {
-  process.stdout.write('\n');
-}, 2000);
+  process.stdout.write('\n')
+}, delay);
